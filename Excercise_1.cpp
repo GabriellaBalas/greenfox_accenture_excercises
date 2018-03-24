f2e6e19baabe3c643ba68cd7167a5e882aa3862e
GabriellaBalas <37745460+GabriellaBalas@users.noreply.github.com>
2018-03-24 17:14:07 +0100

    Create Excercise_1.cpp

---
+++ Excercise_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+float numerator = 0;
+float denominator = 0;
+float odd_average;
+int evens = 0;
+
+void excercise1(int numbers[], int size) {
+    for (int a=0; a<size; a++) {
+        if (numbers[a]%2 == 1) {
+            numerator+=numbers[a];
+            denominator++;
+        }
+        else {
+            evens++;
+        }
+    }
+    if (evens != size) {
+        odd_average = numerator/denominator;
+        cout<<"the average of odd numbers: "<<odd_average<<endl;
+    }
+    else {
+        cout<<"no odd numbers \n";
+    }
+}
